836cde98b2f4a5b610246230b047da4f2c7b05d4
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-15 16:58:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const app = "I don't do much."
 
 function destructivelyAppendKitten(name) {
   kittens.push(name)
+  return kittens
 }
 
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
+  return kittens
 }
 
 function destructivelyRemoveLastKitten() {
   kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
 }
